b1bd1e72bf919e96540193b8bc7a9921280276e8
Mathieu Fourré <mathieu.fourre@cybertestsystems.com>
2022-12-16 12:55:21 +0100

    fix + add minified

--- minify.py
+++ minify.py
@@ -1,36 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
+import runfromvenv
+
+import python_minifier
 import base64
 import zlib
 import marshal
 
-stages = [
-        #(lambda x: f"exec(__import__('marshal').loads({x}))", lambda x: marshal.dumps(compile(x, "", "exec", optimize=2))),
-        (lambda x: f"exec({x})", lambda x: x),
-        (lambda x: f"__import__('zlib').decompress({x})", lambda x: zlib.compress(x.encode('ascii')), 9, 2),
-        #(lambda x: f"__import__('base64').a85decode({x})",lambda x: base64.a85encode(x.encode('ascii'))),
-        #(lambda x: f"eval({x})", lambda x: x),
-        ]
-
-
 with open(f"runfromvenv.py", "rb") as f:
     code = f.read()
     print(f"init: {len(code)}")
+    mini = python_minifier.minify(code, None, remove_annotations=True, remove_pass=True, remove_literal_statements=True, combine_imports=True, hoist_literals=True, rename_locals=True, rename_globals=True, remove_object_base=True, convert_posargs_to_args=True)
+    print(f"mini: {len(mini)}")
 
-for stage in stages:
-    code = stage[0](stage[1](code))
+code = mini.encode('ascii')
+code = zlib.compress(code, 9)
+code = base64.a85encode(code, foldspaces=True)
+code = f"_i=__import__;exec(_i('zlib').decompress(_i('base64').a85decode({code})))"
 
 print(f"after: {len(code)}")
-print(code)
-eval(code)
-
-
-exit()
-
-payload = zlib.compress(marshal.dumps(compile(code, "runfromvenv", "exec", optimize=2)), 9)
-stage2 = marshal.dumps(compile(f"exec(__import__('marshal').loads(__import__('zlib').decompress({payload})))", "2", "eval", optimize=2))
-stage1 = marshal.dumps(compile(f"exec(__import__('marshal').loads(__import__('base64').a85decode({base64.a85encode(stage2)})))", "1", "eval", optimize=2))
-
-print(f"size: {len(str(payload))} -> {len(str(stage1))}")
-cmd = f"exec(__import__('marshal').loads({str(stage1)}))"
-print(cmd)
-eval(cmd)
+with open("minified", "w") as f:
+    f.write(code)
--- runfromvenv.py
+++ runfromvenv.py
@@ -1,28 +1,24 @@
-print("*Magic*")
-exit(0)
+import venv as v, sys as s, os, inspect as i, subprocess as sp
+from os.path import realpath as rp, dirname as dn, join as j, basename as bn, isdir, isfile
+from os import environ as e, execvp as ex
 
-import venv, sys, os, inspect, subprocess
-from os.path import realpath, dirname, join, basename, isdir, isfile
-from os import environ, execvp
+pj_f=rp(next(reversed(i.stack())).frame.f_globals["__file__"])
+pj_d=dn(pj_f)
+v_d=j(pj_d, f".{bn(pj_d)}.venv")
 
-proj_file = realpath(next(reversed(inspect.stack())).frame.f_globals["__file__"])
-proj_dir = dirname(proj_file)
-venv_dir = join(proj_dir, f".venv_{basename(proj_file)}")
+if not isdir(v_d):
+    e['P']=''
+    v.create(v_d, False, False, True, True, None, True)
 
-if not isdir(venv_dir):
-    environ["RUN_PIP_INSTALL"] = "1"
-    venv.create(venv_dir, system_site_packages=False, with_pip=True, upgrade_deps=True)
+v_bin_d=j(v_d, "bin")
+p=bn(rp(s.executable))
 
-venv_bin_dir = join(venv_dir, "bin")
-python = basename(realpath(sys.executable))
+v_py = rp(j(v_bin_d, p))
+if rp(s.executable) != v_py:
+    e={**e,"VIRTUAL_ENV":v_d,"PATH":f"{v_bin_d}:{os.environ['PATH']}"}
+    ex(pj_f, s.argv)
 
-venv_python = realpath(join(venv_bin_dir, python))
-if realpath(sys.executable) != venv_python:
-    environ["VIRTUAL_ENV"] = venv_dir
-    environ["PATH"] = f"{venv_bin_dir}:{os.environ['PATH']}"
-    execvp(proj_file, sys.argv)
-
-requirements = join(proj_dir, "requirements.txt")
-if isfile(requirements) and environ.get("RUN_PIP_INSTALL") == "1":
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'wheel'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', requirements])
+r = j(pj_d, "requirements.txt")
+if isfile(r) and 'P' in e:
+    for a in (('wheel',), ('-r', r)):
+        sp.check_call([s.executable, '-m', 'pip', 'install', *a])
